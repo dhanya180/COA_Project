aec9666a1908f32fa27b85083081faf7a2499afb
sahithi2426 <cs23b048@iittp.ac.in>
2025-02-16 23:11:28 +0530

    sub command

--- main.cpp
+++ main.cpp
@@ -2,30 +2,31 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
-
+using namespace std;
 class Cores {
 public:
-    std::vector<int> registers;
+    vector<int> registers;
     int pc;
     int coreid;
 
     Cores(int cid) : registers(32, 0), pc(0), coreid(cid) {}
 
-    void execute(const std::vector<std::string>& pgm, std::vector<int>& mem) {
+    void execute(const vector<string>& pgm, vector<int>& mem) {
         if (pc >= pgm.size()) return;
         
-        std::istringstream iss(pgm[pc]);
-        std::string opcode, rd_str, rs1_str, rs2_str;
+        istringstream iss(pgm[pc]);
+        string opcode, rd_str, rs1_str, rs2_str;
         iss >> opcode >> rd_str >> rs1_str >> rs2_str;
 
-        int rd = std::stoi(rd_str.substr(1));
-        int rs1 = std::stoi(rs1_str.substr(1));
-        int rs2 = std::stoi(rs2_str.substr(1));
+        int rd = stoi(rd_str.substr(1));
+        int rs1 = stoi(rs1_str.substr(1));
+        int rs2 = stoi(rs2_str.substr(1));
 
         if (opcode == "ADD") {
             registers[rd] = registers[rs1] + registers[rs2];
-        } else if (opcode == "LD") {
-            // TODO: Implement memory instructions
+        } 
+        else if (opcode == "SUB") {
+           registers[rd] = registers[rs1] - registers[rs2];
         }
         pc++;
     }
@@ -33,10 +34,10 @@ public:
 
 class Simulator {
 public:
-    std::vector<int> memory;
+    vector<int> memory;
     int clock;
-    std::vector<Cores> cores;
-    std::vector<std::string> program;
+    vector<Cores> cores;
+    vector<string> program;
 
     Simulator() : memory(4096 / 4, 0), clock(0), cores(4, Cores(0)) {
         for (int i = 0; i < 4; ++i) {
@@ -56,19 +57,22 @@ public:
 
 int main() {
     Simulator sim;
-    sim.program = {"ADD X4 X5 X6", "ADD X7 X8 X9"};
-    sim.cores[0].registers[5] = 3;
-    sim.cores[0].registers[8] = 2;
+    sim.program = {"ADD X4 X5 X6", "SUB X7 X8 X9"};
+    for(int i=0;i<4;i++){
+        sim.cores[i].registers[5] = 3;
+        sim.cores[i].registers[8] = 12;
+    }
+    
     sim.run();
 
     for (int i = 0; i < 4; ++i) {
-        std::cout << "Core " << i << " Registers: ";
+        cout << "Core " << i << " Registers: ";
         for (int j = 0; j < 32; ++j) {
-            std::cout << std::setw(3) << sim.cores[i].registers[j] << " ";
+            cout << setw(3) << sim.cores[i].registers[j] << " ";
         }
-        std::cout << std::endl;
+       cout << endl;
     }
-    std::cout << "Number of clock cycles: " << sim.clock << std::endl;
+    cout << "Number of clock cycles: " << sim.clock << endl;
 
     return 0;
 }
